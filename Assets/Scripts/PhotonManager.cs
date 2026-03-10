0cf59915237546977e66603c52314eb83d4f183b
Dhiraj Sai <burugapallidhiraj@gmail.com>
2026-03-10 12:30:54 +0000

    MultiplayerAdded
    --- URP Fixed for the Build
    --- Photon Multiplayer Added
    --- Create and Join Room Feature

---
+++ Assets/Scripts/PhotonManager.cs
@@ -0,0 +1,85 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PhotonManager : MonoBehaviourPunCallbacks
+{
+    public static PhotonManager Instance;
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // ─── Called right after Supabase login ──────────────
+    public void ConnectToPhoton()
+    {
+        // Use Supabase username as Photon display name
+        PhotonNetwork.NickName = AuthManager.playerName;
+        PhotonNetwork.AutomaticallySyncScene = true;
+
+        Debug.Log("Connecting as: " + PhotonNetwork.NickName);
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Connected to Photon!");
+        PhotonNetwork.JoinLobby();
+    }
+
+    public override void OnJoinedLobby()
+    {
+        SceneManager.LoadScene("LobbyScene");
+    }
+
+    // ─── Create Room ────────────────────────────────────
+    public void CreateRoom(string roomName, int maxPlayers = 4)
+    {
+        RoomOptions options = new RoomOptions
+        {
+            MaxPlayers = (byte)maxPlayers,
+            IsVisible = true,
+            IsOpen = true
+        };
+        PhotonNetwork.CreateRoom(roomName, options);
+    }
+
+    // ─── Join Room ──────────────────────────────────────
+    public void JoinRoom(string roomName)
+    {
+        PhotonNetwork.JoinRoom(roomName);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("Joined room! Loading game...");
+        PhotonNetwork.LoadLevel("GameScene");
+    }
+
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        Debug.Log(newPlayer.NickName + " joined!");
+    }
+
+    public override void OnPlayerLeftRoom(Player otherPlayer)
+    {
+        Debug.Log(otherPlayer.NickName + " left!");
+    }
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        Debug.LogWarning("Disconnected: " + cause);
+        SceneManager.LoadScene("LoginScene");
+    }
+}
